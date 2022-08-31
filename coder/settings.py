4dc9c7f4710be43d0a12d54d6e774fa4cb2ae477
larrieking <larrie4christ@gmail.com>
2022-09-01 00:49:12 +0100

    Initial COMMITMENT

--- coder/settings.py
+++ coder/settings.py
@@ -73,6 +73,23 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'coder.wsgi.application'
 
+TINYMCE_DEFAULT_CONFIG = {
+    "height": "320px",
+    "width": "960px",
+    "menubar": "file edit view insert format tools table help",
+    "plugins": "advlist autolink lists link image charmap print preview anchor searchreplace visualblocks codesample code "
+    "fullscreen insertdatetime media table paste code help wordcount spellchecker",
+    "toolbar": "undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft "
+    "aligncenter alignright alignjustify | outdent indent |  numlist bullist checklist | forecolor "
+    "backcolor casechange permanentpen formatpainter removeformat | pagebreak | charmap emoticons | "
+    "fullscreen  preview save print | insertfile image media pageembed template link anchor codesample | "
+    "a11ycheck ltr rtl | showcomments addcomment code",
+    "custom_undo_redo_levels": 10,
+     # To force a specific language instead of the Django current language.
+}
+
+TINYMCE_SPELLCHECKER = True
+
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
